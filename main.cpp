f14f1434f1abdf16f6e7cc67fb5159a2281dc8cb
dwnajdz <dawidnajdz@gmail.com>
2024-01-22 15:32:59 +0100

    video add

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 // opencv
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
-
-// other
+#include "opencv2/opencv.hpp"
 #include <iostream>
+#include <stdio.h>
 
 using namespace cv;
 
 int main(int, char **)
 {
-    std::string image_path = samples::findFile("data/test.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if (img.empty())
+    std::string filename = "data/test_video.mp4";
+
+    VideoCapture cap;
+    Mat frame;
+
+    cap.open(filename);
+
+    if (!cap.isOpened())
     {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
+        std::cerr << "no video is opened\n";
+        return -1;
     }
 
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-
-    if (k == 's')
+    namedWindow("frame", 1);
+    while (1)
     {
-        imwrite("data/output_test.png", img);
+        cap.read(frame);
+        if (frame.empty())
+        {
+            std::cerr << "frame is empty\n";
+            break;
+        }
+
+        imshow("Live", frame);
+        waitKey(20);
     }
+
+    waitKey(0);
     return 0;
 }
